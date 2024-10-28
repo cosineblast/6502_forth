e004096cbcf7213b4957095fbb18965175a7db12
cosineblast <55855728+cosineblast@users.noreply.github.com>
2024-10-27 22:01:35 -0300

    Move locals to separate file

---
+++ src/locals.h.s
@@ -0,0 +1,23 @@
+
+  ;; I like to have a lot of local variable registers 
+  ;; when writing code.
+
+  ;; Most assembly routines follow the convention of not 
+  ;; cobbling registers local8 to local15
+
+local0 = $00
+local1 = $01
+local2 = $02
+local3 = $03
+local4 = $04
+local5 = $05
+local6 = $06
+local7 = $07
+local8 = $08
+local9 = $09
+local10 = $10
+local11 = $11
+local12 = $12
+local13 = $13
+local14 = $14
+local15 = $15
