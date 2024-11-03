d678d49f20a497d66d9bd670800f6c86af31be0a
cosineblast <55855728+cosineblast@users.noreply.github.com>
2024-11-03 00:45:43 -0300

    Implement basic indirect threaded code

--- src/forth.s
+++ src/forth.s
@@ -1,21 +1,27 @@
-
 .include "locals.h.s"
-
-    ;; <--- DICTIONARY ENTRY (HEADER) ----------------------->
+  ;; <--- DICTIONARY ENTRY (HEADER) ----------------------->
   ;; +------------------------+--------+---------- - - - - +----------- - - - -
 	;; | LINK POINTER           | LENGTH/| NAME	      | DEFINITION
 	;; |			                  | FLAGS  |     	      |
 	;; +--- (2 bytes) ----------+- byte -+- n bytes  - - - - +----------- - - - -
 
 
-  ;; Two values
-  esi = $16
+  .import read_byte
+  .import write_byte
 
-  ;; One value, offset into SP_PAGE
+  .export forth_main
+
+  ;; Two-byte value
+  esi = $10
+
+
+  ;; One-byte value, offset into SP_PAGE
   ;; in the future, we might make this two values,
   ;; for multi-page stacks, but that might make things slow.
-  sp = $18
-  DATA_STACK_PAGE = $02
+  stack_offset = $12
+  DATA_STACK = $2300
+
+
 
 .proc next
 
@@ -41,9 +47,16 @@
     lda (local0),y
     sta local3
 
-    ; jmp *tmp2
-    jmp (local2)
-    rts
+    ; tmp3 = *tmp2
+    ldy #0
+    lda (local2),y
+    sta local0
+    iny
+    lda (local2),y
+    sta local1
+
+    ; jmp *tmp3
+    jmp (local0)
 
 .endproc
 
@@ -52,12 +65,96 @@ DUP:
   .word DUP_code
 
 DUP_code:
-  ldx sp
-  lda DATA_STACK_PAGE, x
+  ldx stack_offset
+  lda DATA_STACK, x
   dex
-  dec sp
-  sta DATA_STACK_PAGE, x
-  jsr next
+  stx stack_offset
+  sta DATA_STACK, x
+  jmp next
+
+ADD:
+  .word ADD_code
+
+ADD_code:
+  ldx stack_offset
+  lda DATA_STACK, x
+  sta local0
+
+  inx
+  stx stack_offset
+  lda DATA_STACK, x
+  clc
+  adc local0
+  sta DATA_STACK, x
+  jmp next
+
+
+DOUBLE:
+  .word docol
+  .word DUP
+  .word ADD
+  .word EXIT
+
+EXIT:
+  .word EXIT_code
+EXIT_code:
+  ; esi = *return_stack
+  ; return_stack += 2
+  pla
+  sta esi+1
+  pla 
+  sta esi
+  jmp next
+
+docol:
+  ; push esi
+  lda esi
+  pha
+  lda esi+1
+  pha
+
+  ; entry = *(esi-2)
+  ; // (implicit from next)
+
+  ; esi = entry + 2
+  lda local2
+  clc
+  adc #2
+  sta esi
+  lda local3
+  adc #0
+  sta esi+1
+
+  ; NEXT
+  jmp next
+
+MAIN_words:
+  .word DOUBLE
+  .word DOUBLE
+  .word RETURN
+
+RETURN:
+  .word RETURN_code
+RETURN_code:
+  rts
+
+forth_main:
+
+  lda #$ff
+  sta stack_offset
+
+  lda #8
+  sta DATA_STACK+$ff
+
+  lda #<MAIN_words
+  sta esi
+
+  lda #>MAIN_words
+  sta esi+1
+
+  jmp next
+
+
 
 
 
--- src/main.s
+++ src/main.s
@@ -1,14 +1,15 @@
-  
   ;; This is the main source file for this project, which is going to be a
   ;; FORTH implementation for 6502 systems.
   ;; The target machine is symon [1], which is a decent virtual 6502 system.
 .include "locals.h.s"
 
-.segment "CODE" 
+.segment "CODE"
 
 .import read_byte
 .import put_byte
 .import setup_io
+
+.import forth_main
 
 start:
   ;; BEGIN SETUP
@@ -16,13 +17,15 @@ start:
   ;; The first thing we do is to allow interrupts to get to the CPU
   cli
 
-  ;; Then, we setup the ACIA 
+  ;; Then, we setup the ACIA
   jsr setup_io
+
 
 
   ;; END SETUP
 
-loop:   
+  jsr forth_main
+loop:
   jsr read_byte
   jsr put_byte
   jmp loop
